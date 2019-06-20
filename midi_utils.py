f555b5b40ce295a8a391b05dd598fbeb127543d6
Matthew Billman <mgbvox@gmail.com>
2019-06-20 09:48:54 -0400

    u_1

---
+++ midi_utils.py
@@ -0,0 +1,36 @@
+# see: https://www.daniweb.com/programming/software-development/code/216976/play-a-midi-music-file-using-pygame
+
+# pip install pygame
+
+
+import pygame
+
+def play_music(music_file):
+    
+    freq =  44100    # audio CD quality
+    bitsize =  -16   # unsigned 16 bit
+    channels = 2    # 1 is mono, 2 is stereo
+    buffer = 1024    # number of samples
+    pygame.mixer.init(freq, bitsize, channels, buffer)
+
+    # optional volume 0 to 1.0
+    pygame.mixer.music.set_volume(0.8)
+    
+    
+    """
+    stream music with mixer.music module in blocking manner
+    this will stream the sound from disk while playing
+    """
+    clock = pygame.time.Clock()
+    try:
+        pygame.mixer.music.load(music_file)
+        print("Music file %s loaded!" % music_file)
+    except pygame.error:
+        print("File %s not found! (%s)" % (music_file, pygame.get_error()))
+        return
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        # check if playback has finished
+        clock.tick(30)
+# pick a midi music file you have ...
+# (if not in working folder use full path)
